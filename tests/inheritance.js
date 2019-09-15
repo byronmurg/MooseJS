bef6201643111be1b6e8a3570665760719f9076b
Byron Murgatroyd <byron.murgatroyd@gmail.com>
2019-09-15 16:05:25 +0100

    Added check for required fields with defaults being inherited

--- tests/inheritance.js
+++ tests/inheritance.js
@@ -10,6 +10,7 @@ Test.section("Inheritance", (test) => {
 		has: {
 			permissive:{ is:"rw", isa:Number },
 			strict:    { is:"ro", isa:Number, required:true },
+			requiredWithDefault: { is:"ro", isa:Number, required:true, default:5 },
 		}
 	})
 	{}
@@ -25,5 +26,6 @@ Test.section("Inheritance", (test) => {
 
 	test.check_exception(`'strict' cannot be deleted`, `Cannot delete required property "strict" of class "Child"`, () => {delete child.strict});
 	test.check_safe(`'permissive' can be deleted`, () => {delete child.permissive});
+	test.check_true(`'requiredWithDefault' has been set correctly`, () => child.requiredWithDefault == 5)
 })
 
